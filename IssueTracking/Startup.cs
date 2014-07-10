0803fe4c2919232e1fb578e0faaebbf578d016ec
Ken Tseng <vul3yo6@hotmail.com>
2014-07-11 00:53:10 +0800

    First Commit
    Signed-off-by: Ken Tseng <vul3yo6@hotmail.com>

---
+++ IssueTracking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IssueTracking.Startup))]
+namespace IssueTracking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
